9b40ee4365a8d2b9880cb0a333757c2095908666
PierreNP <pierre.noppe@essec.edu>
2021-08-10 16:39:44 +0200

    navbar terminée à part Mes events

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :attendances
   resources :events
+  resources :users do
+    resources :events
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
