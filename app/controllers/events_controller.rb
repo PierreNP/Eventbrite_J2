9d116268822027cdab019ce0098d9f009cee8a31
jonathan justman <jojo.justman@gmail.com>
2021-08-15 13:50:35 +0200

    gesion création event par un admin - event automatiquement accepté

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -57,6 +57,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:event).permit(:title, :description, :location, :duration, :price, :start_date)
+      params.require(:event).permit(:title, :description, :location, :duration, :price, :start_date, :is_validated)
     end
 end
